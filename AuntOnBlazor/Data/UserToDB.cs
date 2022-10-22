a67dff9844395cf5afc3046257529f76ec5fd747
bezoksy <96592214+bezoksy@users.noreply.github.com>
2022-10-22 11:07:18 +0300

    Add project files.

---
+++ AuntOnBlazor/Data/UserToDB.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+namespace AuntOnBlazor.Data
+{
+    public class UserToDB
+    {
+        public User currentUser { get; set; }
+        private IMongoCollection<User> _users;
+
+        public UserToDB()
+        {
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+            var dataBase = client.GetDatabase("Users");
+            _users = dataBase.GetCollection<User>("Users");
+        }
+
+        public void AddUser(User user)
+        {
+            _users.InsertOne(user);
+        }
+    }
+}
